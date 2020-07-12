7d338d2a01a77eed048c7a7f0f863c61291f6a1a
jpolacco <jpolacco@gmail.com>
2020-07-12 10:47:39 -0700

    Create MyMain.java

---
+++ MyMain.java
@@ -0,0 +1,9 @@
+public class MyMain {
+
+    public static void main(String[] args) {
+        Doubler doubler = new Doubler();
+
+        System.out.println(doubler.doubleIt(4));
+        System.out.println(doubler.doubleIt(7));
+    }
+}
